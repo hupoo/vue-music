6913a42f009cb779e58a560fd362471f66ec483d
hupoo <352579543@qq.com>
2018-10-09 10:20:04 +0800

    'add'

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Search from 'components/search/search'
+import Rank from 'components/rank/rank'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/Recommend',
+      component: Recommend
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: Singer
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/rank',
+      name: 'Rank',
+      component: Rank
+    }
+  ]
+})
